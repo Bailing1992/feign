18b5388b070f17b7c493e823c93bc0539f2b355b
Kevin Davis <kdavisk6@gmail.com>
2019-05-07 15:30:58 -0400

    Corrected Commit Message on release

--- travis/release.sh
+++ travis/release.sh
@@ -30,7 +30,7 @@ echo "release version is: ${tag} and next snapshot is: ${snapshot}"
 
 # Update the versions, removing the snapshots, then create a new tag for the release, this will
 # start the travis-ci release process.
-./mvnw -B versions:set scm:checkin -DremoveSnapshot -DgenerateBackupPoms=false -Dmessage="\"prepare release ${tag}\"" -DpushChanges=false
+./mvnw -B versions:set scm:checkin -DremoveSnapshot -DgenerateBackupPoms=false -Dmessage="prepare release ${tag}" -DpushChanges=false
 
 # Update the versions to the next snapshot
 ./mvnw -B versions:set scm:checkin -DnewVersion="${snapshot}" -Dmessage="updating versions to next development iteration ${snapshot}" -DpushChanges=false
